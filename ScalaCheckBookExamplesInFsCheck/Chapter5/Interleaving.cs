ba8f63278f79dd90052ee40f0dafb719edb161ae
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-02-02 21:52:11 +0000

    Added chapter 5 example re interleaving

---
+++ ScalaCheckBookExamplesInFsCheck/Chapter5/Interleaving.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ScalaCheckBookExamplesInFsCheck.Chapter5
+{
+    internal static class Interleaving
+    {
+        public static IList<T> Interleave<T>(IList<T> xs, IList<T> ys)
+        {
+            if (xs.Count == 0) return ys;
+            if (ys.Count == 0) return xs;
+            return new[] {xs.First()}
+                .Concat(new[] {ys.First()})
+                .Concat(Interleave(xs.Skip(1).ToList(), ys.Skip(1).ToList()))
+                .ToList();
+        }
+    }
+}
---
+++ ScalaCheckBookExamplesInFsCheck/Chapter5/InterleavingTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FsCheck;
+using FsCheck.Fluent;
+using FsCheckUtils;
+using NUnit.Framework;
+
+namespace ScalaCheckBookExamplesInFsCheck.Chapter5
+{
+    [TestFixture]
+    public class InterleavingTests
+    {
+        private static readonly Config Config = Config.VerboseThrowOnFailure;
+        private static readonly Configuration Configuration = Config.ToConfiguration();
+
+        [Test]
+        public void InterleavePropertyFluent()
+        {
+            Spec
+                .For(Any.OfType<IList<int>>(), Any.OfType<IList<int>>(), (xs, ys) =>
+                {
+                    var res = Interleaving.Interleave(xs, ys);
+                    return xs.Count + ys.Count == res.Count();
+                })
+                .Label("length")
+                .And((xs, ys) =>
+                {
+                    var res = Interleaving.Interleave(xs, ys);
+                    var idxs = Enumerable.Range(0, Math.Min(xs.Count, ys.Count)).ToList();
+                    return xs.SequenceEqual(idxs.Select(idx => res[2 * idx]).Concat(res.Skip(2 * ys.Count)));
+                })
+                .Label("zip xs")
+                .And((xs, ys) =>
+                {
+                    var res = Interleaving.Interleave(xs, ys);
+                    var idxs = Enumerable.Range(0, Math.Min(xs.Count, ys.Count)).ToList();
+                    return ys.SequenceEqual(idxs.Select(idx => res[2 * idx + 1]).Concat(res.Skip(2 * xs.Count)));
+                })
+                .Label("zip ys")
+                .Check(Configuration);
+        }
+    }
+}
