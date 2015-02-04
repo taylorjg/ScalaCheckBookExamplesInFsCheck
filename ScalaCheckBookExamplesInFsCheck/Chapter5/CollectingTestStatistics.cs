b39544660173da84efd9cad4bf79a5561f309194
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-02-04 10:19:37 +0000

    Now for Test (non-fluent) and Property (non-fluent) versions of Prop.collect example

--- ScalaCheckBookExamplesInFsCheck/Chapter5/CollectingTestStatistics.cs
+++ ScalaCheckBookExamplesInFsCheck/Chapter5/CollectingTestStatistics.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using FsCheck;
-//using FsCheck.Fluent;
-//using FsCheckUtils;
 using Microsoft.FSharp.Core;
 using NUnit.Framework;
 using Flinq;
@@ -15,10 +13,9 @@ namespace ScalaCheckBookExamplesInFsCheck.Chapter5
     public class CollectingTestStatistics
     {
         private static readonly Config Config = Config.QuickThrowOnFailure;
-        //private static readonly Configuration Configuration = Config.ToConfiguration();
 
         [Test]
-        public void PropertyFluent()
+        public void Test()
         {
             Check.One(Config, Prop.forAll(Arb.from<List<int>>(), FSharpFunc<List<int>, Property>.FromConverter(xs =>
             {
@@ -26,24 +23,38 @@ namespace ScalaCheckBookExamplesInFsCheck.Chapter5
                 {
                     return Prop.forAll(Arb.fromGen(Gen.choose(n, xs.Count)), FSharpFunc<int, Property>.FromConverter(m =>
                     {
-                        var slice1 = xs.Slice(n, m).ToList();
-                        string label1;
-                        switch (slice1.Count)
+                        var slice = xs.Slice(n, m).ToList();
+                        string label;
+                        switch (slice.Count)
                         {
-                            case 0:
-                                label1 = "none";
-                                break;
-                            case 1:
-                                label1 = "one";
-                                break;
-                            default:
-                                label1 = (slice1.Count == xs.Count) ? "whole" : "part";
-                                break;
+                            case 0: label = "none"; break;
+                            case 1: label = "one"; break;
+                            default: label = (slice.Count == xs.Count) ? "whole" : "part"; break;
                         }
-                        return Prop.collect<string, bool>(label1).Invoke(xs.ContainsSlice(slice1));
+                        return Prop.collect<string, bool>(label).Invoke(xs.ContainsSlice(slice));
                     }));
                 }));
             })));
+        }
+
+        [FsCheck.NUnit.Property]
+        public Property Property(List<int> xs)
+        {
+            return Prop.forAll(Arb.fromGen(Gen.choose(0, xs.Count - 1)), FSharpFunc<int, Property>.FromConverter(n =>
+            {
+                return Prop.forAll(Arb.fromGen(Gen.choose(n, xs.Count)), FSharpFunc<int, Property>.FromConverter(m =>
+                {
+                    var slice = xs.Slice(n, m).ToList();
+                    string label;
+                    switch (slice.Count)
+                    {
+                        case 0: label = "none"; break;
+                        case 1: label = "one"; break;
+                        default: label = (slice.Count == xs.Count) ? "whole" : "part"; break;
+                    }
+                    return Prop.collect<string, bool>(label).Invoke(xs.ContainsSlice(slice));
+                }));
+            }));
         }
     }
 }
