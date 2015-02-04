0926f2588e0b9fc8fa50c734017052b578dfa04b
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-02-04 09:48:36 +0000

    Got Prop.classify and Prop.collect covered

--- ScalaCheckBookExamplesInFsCheck/Chapter5/CollectingTestStatistics.cs
+++ ScalaCheckBookExamplesInFsCheck/Chapter5/CollectingTestStatistics.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 using FsCheck;
-using FsCheck.Fluent;
-using FsCheckUtils;
+//using FsCheck.Fluent;
+//using FsCheckUtils;
 using Microsoft.FSharp.Core;
 using NUnit.Framework;
+using Flinq;
 
 namespace ScalaCheckBookExamplesInFsCheck.Chapter5
 {
@@ -13,56 +15,35 @@ namespace ScalaCheckBookExamplesInFsCheck.Chapter5
     public class CollectingTestStatistics
     {
         private static readonly Config Config = Config.QuickThrowOnFailure;
-        private static readonly Configuration Configuration = Config.ToConfiguration();
+        //private static readonly Configuration Configuration = Config.ToConfiguration();
 
         [Test]
-        public void TestFluent()
+        public void PropertyFluent()
         {
-            Spec
-                .ForAny((int n) => n + n == 2*n)
-                .Classify(x => x%2 == 0, "even")
-                .Classify(x => x%2 != 0, "odd")
-                .Classify(x => x < 0, "neg")
-                .Classify(x => x >= 0, "pos")
-                .Classify(x => Math.Abs(x) > 50, "large")
-                .Check(Configuration);
-        }
-
-        [Test]
-        public void Test()
-        {
-            var body = FSharpFunc<int, Property>.FromConverter(n =>
-                Prop.classify<Property>(n%2 == 0, "even")
-                    .Invoke(Prop.classify<Property>(n%2 != 0, "odd")
-                        .Invoke(Prop.classify<Property>(n < 0, "neg")
-                            .Invoke(Prop.classify<Property>(n >= 0, "pos")
-                                .Invoke(Prop.classify<Property>(Math.Abs(n) > 50, "large")
-                                    .Invoke(Prop.ofTestable(n + n == 2*n)))))));
-            Check.One(Config, body);
-        }
-
-        [FsCheck.NUnit.Property]
-        public Property PropertyFluent(int n)
-        {
-            return Spec
-                .For(Any.Value(n), i => n + n == 2*n)
-                .Classify(x => x%2 == 0, "even")
-                .Classify(x => x%2 != 0, "odd")
-                .Classify(x => x < 0, "neg")
-                .Classify(x => x >= 0, "pos")
-                .Classify(x => Math.Abs(x) > 50, "large")
-                .Build();
-        }
-
-        [FsCheck.NUnit.Property]
-        public Property Property(int n)
-        {
-            return Prop.classify<Property>(n%2 == 0, "even")
-                .Invoke(Prop.classify<Property>(n%2 != 0, "odd")
-                    .Invoke(Prop.classify<Property>(n < 0, "neg")
-                        .Invoke(Prop.classify<Property>(n >= 0, "pos")
-                            .Invoke(Prop.classify<Property>(Math.Abs(n) > 50, "large")
-                                .Invoke(Prop.ofTestable(n + n == 2*n))))));
+            Check.One(Config, Prop.forAll(Arb.from<List<int>>(), FSharpFunc<List<int>, Property>.FromConverter(xs =>
+            {
+                return Prop.forAll(Arb.fromGen(Gen.choose(0, xs.Count - 1)), FSharpFunc<int, Property>.FromConverter(n =>
+                {
+                    return Prop.forAll(Arb.fromGen(Gen.choose(n, xs.Count)), FSharpFunc<int, Property>.FromConverter(m =>
+                    {
+                        var slice1 = xs.Slice(n, m).ToList();
+                        string label1;
+                        switch (slice1.Count)
+                        {
+                            case 0:
+                                label1 = "none";
+                                break;
+                            case 1:
+                                label1 = "one";
+                                break;
+                            default:
+                                label1 = (slice1.Count == xs.Count) ? "whole" : "part";
+                                break;
+                        }
+                        return Prop.collect<string, bool>(label1).Invoke(xs.ContainsSlice(slice1));
+                    }));
+                }));
+            })));
         }
     }
 }
