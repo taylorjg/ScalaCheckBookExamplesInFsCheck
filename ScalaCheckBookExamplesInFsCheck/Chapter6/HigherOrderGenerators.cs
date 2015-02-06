1857f4f8485b13f4a70b7d77ebbf66a62ee45569
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-02-06 13:55:51 +0000

    Added examples re frequency, sequence and oneOf

--- ScalaCheckBookExamplesInFsCheck/Chapter6/HigherOrderGenerators.cs
+++ ScalaCheckBookExamplesInFsCheck/Chapter6/HigherOrderGenerators.cs
@@ -1,13 +1,75 @@
-﻿using NUnit.Framework;
+﻿using System;
+using FsCheck;
+using FsCheck.Fluent;
+using FsCheckUtils;
+using Microsoft.FSharp.Collections;
+using Microsoft.FSharp.Core;
+using NUnit.Framework;
+using ScalaCheckBookExamplesInFsCheck.Utils;
 
 namespace ScalaCheckBookExamplesInFsCheck.Chapter6
 {
+    using Property = Gen<Rose<Result>>;
+    
     [TestFixture]
     public class HigherOrderGenerators
     {
+        private static readonly Config Config = Config.QuickThrowOnFailure;
+
         [Test]
-        public void Test()
+        public void SequenceTest()
         {
+            var numbers = Gen.sequence(ListModule.OfSeq(new[]
+            {
+                Gen.choose(1, 10),
+                Gen.constant(20),
+                Gen.constant(30)
+            }));
+            numbers.DumpSamples();
+        }
+
+        [Test]
+        public void FrequencyTest()
+        {
+            var evenNumbersGen = from n in Gen.choose(2, 100000) select 2*n;
+            var oddNumbersGen = from n in Gen.choose(0, 100000) select 2*n + 1;
+            var numberGen = Gen.frequency(new[]
+            {
+                Tuple.Create(1, oddNumbersGen),
+                Tuple.Create(2, evenNumbersGen),
+                Tuple.Create(4, Gen.constant(0))
+            });
+
+            var property = Prop.forAll(Arb.fromGen(numberGen), FSharpFunc<int, Property>.FromConverter(n =>
+            {
+                string l;
+                if (n == 0) l = "one";
+                else if (n%2 == 0) l = "even";
+                else l = "odd";
+                return Prop.collect<string, bool>(l).Invoke(true);
+            }));
+
+            Check.One(Config, property);
+            Check.One(Config.WithMaxTest(1000), property);
+            Check.One(Config.WithMaxTest(10000), property);
+        }
+
+        [Test]
+        public void OneOfTest()
+        {
+            var genNotZero = Gen.oneof(new[] {Gen.choose(-10, -1), Gen.choose(1, 10)});
+            genNotZero.DumpSamples();
+
+            var genVowel = Gen.oneof(new[]
+            {
+                Gen.constant('a'),
+                Gen.constant('e'),
+                Gen.constant('i'),
+                Gen.constant('o'),
+                Gen.constant('u'),
+                Gen.constant('y')
+            });
+            genVowel.DumpSamples();
         }
     }
 }
