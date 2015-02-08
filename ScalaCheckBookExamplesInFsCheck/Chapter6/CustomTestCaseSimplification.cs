edda2877e8b2a9b604fe65b36c8486002892ecfe
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-02-08 13:29:40 +0000

    Done more work on the simplification example

--- ScalaCheckBookExamplesInFsCheck/Chapter6/CustomTestCaseSimplification.cs
+++ ScalaCheckBookExamplesInFsCheck/Chapter6/CustomTestCaseSimplification.cs
@@ -7,18 +7,51 @@ using ScalaCheckBookExamplesInFsCheck.Utils;
 
 namespace ScalaCheckBookExamplesInFsCheck.Chapter6
 {
-    internal abstract class Expression
+    public abstract class Expression
     {
         public override string ToString()
         {
             return Show();
         }
 
+        public Expression Rewrite()
+        {
+            return Match(
+                eConst => eConst as Expression,
+                eAdd =>
+                {
+                    // ReSharper disable ConvertIfStatementToReturnStatement
+                    if (eAdd.Expression1.Equals(eAdd.Expression2)) return new Mul(new Const(2), eAdd.Expression1);
+                    if (eAdd.Expression1.Equals(new Const(0))) return eAdd.Expression2;
+                    if (eAdd.Expression2.Equals(new Const(0))) return eAdd.Expression1;
+                    return eAdd;
+                    // ReSharper restore ConvertIfStatementToReturnStatement
+                },
+                eMul =>
+                {
+                    // ReSharper disable ConvertIfStatementToReturnStatement
+                    if (eMul.Expression1.Equals(new Const(0))) return new Const(0);
+                    if (eMul.Expression2.Equals(new Const(0))) return new Const(0);
+                    if (eMul.Expression1.Equals(new Const(1))) return eMul.Expression2;
+                    if (eMul.Expression2.Equals(new Const(1))) return eMul.Expression1;
+                    return eMul;
+                    // ReSharper restore ConvertIfStatementToReturnStatement
+                });
+        }
+
         public abstract int Eval();
         public abstract string Show();
+
+        private T Match<T>(Func<Const, T> constFunc, Func<Add, T> addFunc, Func<Mul, T> mulFunc)
+        {
+            if (this is Const) return constFunc(this as Const);
+            if (this is Add) return addFunc(this as Add);
+            if (this is Mul) return mulFunc(this as Mul);
+            throw new InvalidOperationException("Unknown expression type");
+        }
     }
 
-    internal class Const : Expression
+    public class Const : Expression
     {
         public Const(int value)
         {
@@ -40,10 +73,24 @@ namespace ScalaCheckBookExamplesInFsCheck.Chapter6
             return Convert.ToString(Value);
         }
 
+        public override bool Equals(object rhs)
+        {
+            if (rhs == null) return false;
+            if (ReferenceEquals(this, rhs)) return true;
+            var other = rhs as Const;
+            if (other == null) return false;
+            return (Value == other.Value);
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
         private readonly int _value;
     }
 
-    internal class Add : Expression
+    public class Add : Expression
     {
         public Add(Expression expression1, Expression expression2)
         {
@@ -71,11 +118,25 @@ namespace ScalaCheckBookExamplesInFsCheck.Chapter6
             return string.Format("({0} + {1})", Expression1.Show(), Expression2.Show());
         }
 
+        public override bool Equals(object rhs)
+        {
+            if (rhs == null) return false;
+            if (ReferenceEquals(this, rhs)) return true;
+            var other = rhs as Add;
+            if (other == null) return false;
+            return (Expression1 == other.Expression1 && Expression2 == other.Expression2);
+        }
+
+        public override int GetHashCode()
+        {
+            return Expression1.GetHashCode() + Expression2.GetHashCode();
+        }
+
         private readonly Expression _expression1;
         private readonly Expression _expression2;
     }
 
-    internal class Mul : Expression
+    public class Mul : Expression
     {
         public Mul(Expression expression1, Expression expression2)
         {
@@ -103,6 +164,20 @@ namespace ScalaCheckBookExamplesInFsCheck.Chapter6
             return string.Format("({0} * {1})", Expression1.Show(), Expression2.Show());
         }
 
+        public override bool Equals(object rhs)
+        {
+            if (rhs == null) return false;
+            if (ReferenceEquals(this, rhs)) return true;
+            var other = rhs as Mul;
+            if (other == null) return false;
+            return (Expression1 == other.Expression1 && Expression2 == other.Expression2);
+        }
+
+        public override int GetHashCode()
+        {
+            return Expression1.GetHashCode() * Expression2.GetHashCode();
+        }
+
         private readonly Expression _expression1;
         private readonly Expression _expression2;
     }
@@ -110,10 +185,21 @@ namespace ScalaCheckBookExamplesInFsCheck.Chapter6
     [TestFixture]
     public class CustomTestCaseSimplification
     {
+        private static readonly Config Config = Config.VerboseThrowOnFailure;
+
+        [Test]
+        public void Sample()
+        {
+            GenExpr.DumpSamples();
+        }
+
         [Test]
         public void Test()
         {
-            GenExpr.DumpSamples();
+            var arb = Arb.fromGen(GenExpr);
+            var body = FSharpFunc<Expression, bool>.FromConverter(e => e.Rewrite().Eval() == e.Eval());
+            var property = Prop.forAll(arb, body);
+            Check.One(Config, property);
         }
 
         private static Gen<Expression> GenExpr
